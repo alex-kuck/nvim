d8a3944d4adc4fe01f5ff59b9e90d6da88e73dc1
Alex Kucksdorf <akucksdorf@it-economics.de>
2026-03-19 12:34:51 +0100

    Use repo root for snacks picker keybindings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,11 +1,30 @@
+local function repo_root()
+  local git = vim.fs.find({ ".git" }, { upward = true })[1]
+  return git and vim.fs.dirname(git) or vim.fn.getcwd()
+end
+
+local function set_picker_keys()
+  vim.keymap.set("n", "<leader>/", function()
+    Snacks.picker.grep({ cwd = repo_root() })
+  end, { desc = "Grep (Repo Root)" })
+
+  vim.keymap.set("n", "<leader><space>", function()
+    Snacks.picker.files({ cwd = repo_root() })
+  end, { desc = "Find Files (Repo Root)" })
+
+  vim.keymap.set("n", "<leader>ff", function()
+    Snacks.picker.files({ cwd = repo_root() })
+  end, { desc = "Find Files (Repo Root)" })
+end
+
 return {
   {
     "nvim-telescope/telescope.nvim",
     version = "*",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      -- optional but recommended
       { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     },
+    init = set_picker_keys,
   },
 }
