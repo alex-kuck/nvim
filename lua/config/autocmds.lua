7729e431ed0e032a009de78f01c8a26b0440d7c1
Alex Kucksdorf <akucksdorf@it-economics.de>
2026-04-01 13:41:09 +0200

    feat: Add autocmds to write files automatically when focus is lost

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -41,3 +41,53 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     normalize_yaml_tabs(args.buf)
   end,
 })
+
+-- Shortcuts
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+-- ====================================================================
+-- General Autocommands
+-- ====================================================================
+local general = augroup("General", { clear = true })
+
+-- --------------------------------------------------------------------
+-- Disable Readonly Prompt (E.g., W10 Warning)
+-- --------------------------------------------------------------------
+-- Automatically removes the readonly flag if triggered by FileChangedRO.
+-- Useful when editing files restored or reopened from external changes.
+autocmd("FileChangedRO", {
+  callback = function()
+    vim.bo.readonly = false
+  end,
+  group = general,
+  desc = "Automatically disable readonly mode on change",
+})
+
+-- --------------------------------------------------------------------
+-- Auto Save on Focus Lost or Buffer Leave
+-- --------------------------------------------------------------------
+-- Automatically saves changes when you leave a buffer or Neovim loses focus.
+-- Only triggers for normal file buffers (not terminals or help).
+autocmd({ "FocusLost", "BufLeave", "BufWinLeave", "InsertLeave" }, {
+  callback = function()
+    if vim.bo.filetype ~= "" and vim.bo.buftype == "" and vim.bo.modified then
+      vim.cmd("silent! w")
+    end
+  end,
+  group = general,
+  desc = "Auto-save modified buffers on focus or mode change",
+})
+
+-- --------------------------------------------------------------------
+-- Notify When File is Reloaded
+-- --------------------------------------------------------------------
+-- When an external change causes the file to reload, this provides
+-- a visible notification for user feedback.
+autocmd("FileChangedShellPost", {
+  callback = function()
+    vim.notify("File reloaded automatically", vim.log.levels.INFO, { title = "nvim" })
+  end,
+  group = general,
+  desc = "Notify user when file is reloaded automatically",
+})
